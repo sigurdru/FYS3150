1f586f8de29b1887a7e486bcb09bdb79ce9212a6
Sigurd Rustad <sigurdsr@gmail.com>
2020-08-28 11:58:29 +0200

    fix write to file

--- Project1/code/lineq.cpp
+++ Project1/code/lineq.cpp
@@ -1,19 +1,22 @@
 #include "lineq.hpp"
 #include <iostream>
 #include <cmath>
-// #include <stdlib.h> // use for writing to file?
+#include <fstream>
 
 void Lineq::write_to_file (std::string fname) {
-    std::cout << "x,computed,exact,rel_error,log10(error)" << std::endl;
+    std::ofstream myfile;
     double x_val, comp_val, exact_val, rel_error, log_error;
+    myfile.open (fname);
+    myfile << "x,computed,exact,rel_error,log10(error)" << std::endl;
     for (int i=0; i<m_n; i++) {
         x_val = m_x[i];
         comp_val = m_v[i];
         exact_val = m_exact[i];
-        rel_error = fabs((comp_val - exact_val)/exact_val);
+        rel_error = fabs((comp_val - exact_val) / exact_val);
         log_error = log10(rel_error);
-        std::cout << x_val << "," << comp_val << ","
-            << exact_val << "," << rel_error << ","
-            << log_error << std::endl;
+        myfile << x_val << "," << comp_val << ","
+               << exact_val << "," << rel_error << ","
+               << log_error << std::endl;
     }
+    myfile.close();
 }
