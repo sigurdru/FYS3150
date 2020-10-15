32410771e07b56bbf86fed24648e6f0c41b753d0
Sigurd Rustad <sigurdsr@gmail.com>
2020-10-15 15:12:24 +0200

    work on code

--- Project3/code/main.cpp
+++ Project3/code/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[]) {
     std::string input_file;           // input file
     std::string output_file;          // output file
     int num_bodies;                   // number of planets
-    double *x, *y, *z, *vx, *vy, *vz; // To store initial conditions for each planet.
-    double *mass;                     // Store mass of planets.
+    double x, y, z, vx, vy, vz;       // To store initial conditions for each planet.
+    double mass;                      // Store mass of planets.
     double dt;                        // Time between each step
     int N;                            // Number of integration points
     if (argc < 3) {
@@ -26,28 +26,18 @@ int main(int argc, char* argv[]) {
 
     const char* input_file_char = input_file.c_str();
     FILE *init_file = fopen(input_file_char, "r"); //open file with initial conditions
+    
+    SolarSystem our_system;
     fscanf(init_file, "%i %lf %i", &num_bodies, &dt, &N);
-    x = new double[num_bodies];
-    y = new double[num_bodies];
-    z = new double[num_bodies];
-    vx = new double[num_bodies];
-    vy = new double[num_bodies];
-    vz = new double[num_bodies];
-    mass = new double[num_bodies];
+    
     for (int i = 0; i < num_bodies; i++) {
         fscanf(init_file, "%lf %lf %lf %lf %lf %lf %lf", 
-        &mass[i], &x[i], &y[i], &z[i], &vx[i], &vy[i], &vz[i]);
+        &mass, &x, &y, &z, &vx, &vy, &vz);
+        our_system.createCelestialBody(
+            vec3(x, y, z),
+            vec3(vx*M_PI, vy*M_PI, vz*M_PI), 
+            mass);
     }
     fclose(init_file);
-
-
-    SolarSystem our_system;
-    for (int i = 0; i<num_bodies; i++){
-      our_system.createCelestialBody(
-          vec3(x[i], x[i], x[i]),
-          vec3(vx[i]*M_PI, vy[i]*M_PI, vz[i]*M_PI),
-          mass[i]);
-    }
-
 
 }
