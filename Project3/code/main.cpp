384a7aff217093eeb5b403ab2d0553c391e7c9b0
Sigurd Rustad <sigurdsr@gmail.com>
2020-10-16 10:56:10 +0200

    work on main.cpp

--- Project3/code/main.cpp
+++ Project3/code/main.cpp
@@ -18,19 +18,23 @@ int main(int argc, char* argv[]) {
         std::cout << "Please include input and output file" << std::endl;
     }
     else {
+        // Store input and output file
         output_file = "../output/";
         input_file = "../input/";
         input_file.append(argv[1]);
         output_file.append(argv[2]).append(".cvs");
     }
 
-    const char* input_file_char = input_file.c_str();
+    const char* input_file_char = input_file.c_str(); //fopen takes char* so we convert
     FILE *init_file = fopen(input_file_char, "r"); //open file with initial conditions
     
+    //initialize our system
     SolarSystem our_system;
+    //store number of bodies, dt, and number of time steps
     fscanf(init_file, "%i %lf %i", &num_bodies, &dt, &N);
     
     for (int i = 0; i < num_bodies; i++) {
+        //Here we store the initial conditions and masses
         fscanf(init_file, "%lf %lf %lf %lf %lf %lf %lf", 
         &mass, &x, &y, &z, &vx, &vy, &vz);
         our_system.createCelestialBody(
@@ -38,6 +42,10 @@ int main(int argc, char* argv[]) {
             vec3(vx*M_PI, vy*M_PI, vz*M_PI), 
             mass);
     }
-    fclose(init_file);
-
+    fclose(init_file);  //close file with initial conditions
+    std::vector<CelestialBody> &bodies = our_system.bodies();
+    for(int i = 0; i<num_bodies; i++) {
+        CelestialBody &body = bodies[i]; // Reference to this body
+        std::cout << "The position of this object is " << body.position << " with velocity " << body.velocity << std::endl;
+    }
 }
