f24195ed8583ee746f08d094cd41eeb6fedffacf
Vegard Falmaar <vegardfa@student.matnat.uio.no>
2020-10-16 13:09:04 +0200

    Initial conditions

--- Project3/code/main.cpp
+++ Project3/code/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char* argv[]) {
     int N;                            // Number of integration points
     if (argc < 3) {
         std::cout << "Please include input and output file" << std::endl;
+        exit(1);
     }
     else {
         // Store input and output file
@@ -25,21 +26,45 @@ int main(int argc, char* argv[]) {
         output_file.append(argv[2]).append(".cvs");
     }
 
+    // std::vector<char> planets;
+    // char planet[20], date[11];
+
     const char* input_file_char = input_file.c_str(); //fopen takes char* so we convert
     FILE *init_file = fopen(input_file_char, "r"); //open file with initial conditions
     
     //initialize our system
     SolarSystem our_system;
     //store number of bodies, dt, and number of time steps
-    fscanf(init_file, "%i %lf %i", &num_bodies, &dt, &N);
+    // fscanf(init_file, "%i %lf %i", &num_bodies, &dt, &N);
+    fscanf(init_file, "%i", &num_bodies);
+
+    std::cout << num_bodies << std::endl;
     
     for (int i = 0; i < num_bodies; i++) {
         //Here we store the initial conditions and masses
-        fscanf(init_file, "%lf %lf %lf %lf %lf %lf %lf", 
-        &mass, &x, &y, &z, &vx, &vy, &vz);
+        // fscanf(init_file, "%lf %lf %lf %lf %lf %lf %lf", 
+        // &mass, &x, &y, &z, &vx, &vy, &vz);
+        // fscanf
+        // (
+            // init_file, 
+            // "%s %s %lf %lf %lf %lf %lf %lf", 
+            // planet, date, &x, &y, &z, &vx, &vy, &vz
+        // );
+        // std::cout << i << " " << num_bodies << std::endl;
+        // std::cout << planet << date << std::endl;
+        // std::cout << x << std::endl << std::endl;
+
+        fscanf
+        (
+            init_file, 
+            "%lf %lf %lf %lf %lf %lf %lf", 
+            &mass, &x, &y, &z, &vx, &vy, &vz
+        );
+
+
         our_system.createCelestialBody(
             vec3(x, y, z),
-            vec3(vx*M_PI, vy*M_PI, vz*M_PI), 
+            vec3(vx, vy, vz)*365, 
             mass);
     }
     fclose(init_file);  //close file with initial conditions
--- Project3/input/raw/create_init_cond.sh
+++ Project3/input/raw/create_init_cond.sh
@@ -13,11 +13,18 @@ bodies=(
 )
 
 outfname="../planets_IC.csv"
-echo planet, date, x, y, z, vx, vy, vz > $outfname
+num_bodies=${#bodies[@]}
+echo $num_bodies > $outfname
+# echo planet, date, x, y, z, vx, vy, vz >> $outfname
 
 for body in ${bodies[@]}; do
     fname="${body}.csv"
     line=`cat $fname | head -n 23 | tail -n 1 | cut -d"," -f2-`
-    line=`echo $line | cut -d" " -f2-2,4-`
-    echo ${body}, ${line} >> $outfname
+    # line1=`echo $line | cut -d" " -f2`
+    line2=`echo $line | cut -d" " -f4-`
+    # echo ${body}, ${line1}, ${line2} #>> $outfname
+
+    mass=`cat masses_in_kg.txt | grep $body | cut -d" " -f2`
+
+    echo $mass ${line2} >> $outfname
 done
