e770754afa610bf7bf44bc53b266f1287c35f4b7
Sigurd Rustad <sigurdsr@gmail.com>
2020-11-23 11:53:31 +0100

    start code

--- Project4/code/ising.hpp
+++ Project4/code/ising.hpp
@@ -1,11 +1,12 @@
 #ifndef ISING_HPP
 #define ISING_HPP
 
-class Simple_Lattice {
+class Analytical_2D {
     protected:
-
+        double m_Z;
+        double m_E_exp;
     public:
-    
+        double partition_function(double T);
 };
 
 #endif //ISING_HPP
